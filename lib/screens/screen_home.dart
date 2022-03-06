1526aa5feba6d3098047a395d35cbac60b43b07e
Vivek K J <vivekkj2004@gmail.com>
2022-03-06 16:43:35 +0530

    [UI] -- Update UI and add a stop button

--- lib/screens/screen_home.dart
+++ lib/screens/screen_home.dart
@@ -8,51 +8,97 @@ class ScreenHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Morse Flasher")),
-      body: Center(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: const Color(0xfff1efe3),
+      // appBar: AppBar(title: const Text("Morse Flasher")),
+      body: SafeArea(
+        child: Center(
+            child: Padding(
+          padding: const EdgeInsets.all(10.0),
           child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: messageController,
-              decoration: const InputDecoration(
-                  border: OutlineInputBorder(), hintText: "Message"),
-              maxLines: 15,
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              children: [
+                const Image(image: AssetImage('assets/images/front-image.png')),
+                TextField(
+                  controller: messageController,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          gapPadding: 50,
+                          borderSide:
+                              const BorderSide(color: Color(0xff3f3d56)),
+                          borderRadius: BorderRadius.circular(25.0)),
+                      hintText: "Message"),
+                  maxLines: 15,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () async {
+                        final message = messageController.text;
+                        final encrypted = textToMorse(message.toLowerCase());
+                        if (await TorchLight.isTorchAvailable()) {
+                          blinkMorse(encrypted);
+                        } else {
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext ctx) {
+                                return AlertDialog(
+                                  title: const Text("Error"),
+                                  content: const Text(
+                                      "Can't Access your Camera.. Please try again later.."),
+                                  actions: [
+                                    TextButton(
+                                        onPressed: () {
+                                          Navigator.pop(ctx);
+                                        },
+                                        child: const Text("OK"))
+                                  ],
+                                );
+                              });
+                        }
+                      },
+                      child: const Icon(Icons.flashlight_on),
+                      style: ButtonStyle(
+                          padding: MaterialStateProperty.all<EdgeInsets>(
+                              const EdgeInsets.all(25)),
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              const Color(0xfff1efe3)),
+                          foregroundColor: MaterialStateProperty.all<Color>(
+                              const Color(0xff6c63ff)),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50.0),
+                          ))),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: const Icon(Icons.stop_circle),
+                      style: ButtonStyle(
+                          padding: MaterialStateProperty.all<EdgeInsets>(
+                              const EdgeInsets.all(25)),
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              const Color(0xfff1efe3)),
+                          foregroundColor: MaterialStateProperty.all<Color>(
+                              Color.fromARGB(255, 255, 99, 107)),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50.0),
+                          ))),
+                    ),
+                  ],
+                )
+              ],
             ),
-            const SizedBox(
-              height: 15,
-            ),
-            ElevatedButton.icon(
-                onPressed: () async {
-                  final message = messageController.text;
-                  final encrypted = textToMorse(message.toLowerCase());
-                  if (await TorchLight.isTorchAvailable()) {
-                    blinkMorse(encrypted);
-                  } else {
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext ctx) {
-                          return AlertDialog(
-                            title: const Text("Error"),
-                            content: const Text(
-                                "Can't Access your Camera.. Please try again later.."),
-                            actions: [
-                              TextButton(
-                                  onPressed: () {
-                                    Navigator.pop(ctx);
-                                  },
-                                  child: const Text("OK"))
-                            ],
-                          );
-                        });
-                  }
-                },
-                icon: const Icon(Icons.flashlight_on),
-                label: const Text("Flash the Message"))
-          ],
-        ),
-      )),
+          ),
+        )),
+      ),
     );
   }
 }
